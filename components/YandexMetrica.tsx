c298bb54ce616bf780cf0348634dcfd9d692f81f
AMAN GAIN <amanskbs@gmail.com>
2026-07-27 22:41:24 +0530

    Render Yandex Metrica tag in page HTML

--- components/YandexMetrica.tsx
+++ components/YandexMetrica.tsx
@@ -1,39 +1,39 @@
-"use client";
-
-import Script from "next/script";
-
 const YANDEX_METRICA_TAG_ID = 111062806;
+
+const yandexMetricaCode = `
+  (function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for(var j=0;j<document.scripts.length;j++){
+      if(document.scripts[j].src===r){return;}
+    }
+    k=e.createElement(t);
+    a=e.getElementsByTagName(t)[0];
+    k.async=1;
+    k.src=r;
+    a.parentNode.insertBefore(k,a);
+  })(window,document,"script","https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRICA_TAG_ID}","ym");
+
+  ym(${YANDEX_METRICA_TAG_ID},"init",{
+    ssr:true,
+    webvisor:true,
+    clickmap:true,
+    ecommerce:"dataLayer",
+    referrer:document.referrer,
+    url:location.href,
+    accurateTrackBounce:true,
+    trackLinks:true
+  });
+`;
 
 export default function YandexMetrica() {
   return (
     <>
-      <Script id="yandex-metrica" strategy="afterInteractive">
-        {`
-          (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j=0;j<document.scripts.length;j++) {
-              if (document.scripts[j].src===r) return;
-            }
-            k=e.createElement(t);
-            a=e.getElementsByTagName(t)[0];
-            k.async=1;
-            k.src=r;
-            a.parentNode.insertBefore(k,a);
-          })(window,document,"script","https://mc.yandex.ru/metrika/tag.js?id=${YANDEX_METRICA_TAG_ID}","ym");
-
-          ym(${YANDEX_METRICA_TAG_ID}, "init", {
-            ssr: true,
-            webvisor: true,
-            clickmap: true,
-            ecommerce: "dataLayer",
-            referrer: document.referrer,
-            url: location.href,
-            accurateTrackBounce: true,
-            trackLinks: true
-          });
-        `}
-      </Script>
+      <script
+        id="yandex-metrica"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{ __html: yandexMetricaCode }}
+      />
 
       <noscript>
         {/* eslint-disable-next-line @next/next/no-img-element */}
