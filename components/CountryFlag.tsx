a06464e7334ab08a3c447bb5a7c19b2783e6b4a8
AMAN GAIN <amanskbs@gmail.com>
2026-08-23 00:21:47 +0530

    Bundle country flags locally

--- components/CountryFlag.tsx
+++ components/CountryFlag.tsx
@@ -5,6 +5,5 @@ export default function CountryFlag({ region }: { region?: string | null }) {
 
   if (!code) return <span aria-hidden="true">🌐</span>;
 
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img src={`https://flagcdn.com/20x15/${code}.png`} alt="" width="20" height="15" className="inline-block rounded-[2px] object-cover" />;
+  return <span aria-hidden="true" className={`fi fi-${code} inline-block rounded-[2px]`} />;
 }
